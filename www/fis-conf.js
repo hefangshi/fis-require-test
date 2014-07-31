96f79b0be7a63eac9ff57e7a02bd9d57cf7ee9a0
hefangshi <hefangshi@gmail.com>
2014-07-31 11:15:30 +0800

    update

--- www/fis-conf.js
+++ www/fis-conf.js
@@ -1,6 +1,11 @@
 fis.config.set('roadmap.domain', 'http://127.0.0.1:8080');
 fis.config.set('namespace', 'common');
-
+fis.config.set('roadmap.path', [
+    {
+        reg:"**.html",
+        useMap:true
+    }
+]);
 fis.config.set('pack',{
     'pkg/zrender.js' : [
         '/js/zrender/**'
@@ -16,6 +21,9 @@ fis.config.set('modules.preprocessor.html', 'requirejs');
 
 fis.config.set('settings.preprocessor.requirejs', {
     baseUrl: './js',
+    paths: {
+        'jquery' : 'lib/jquery'
+    },
     packages: [
         {
             name: 'zrender',
